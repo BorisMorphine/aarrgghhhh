065acef27071043119f3bbc1acabc7745a346673
BorisMorphine <123861127+BorisMorphine@users.noreply.github.com>
2024-06-07 00:20:07 -0500

    Update init.sh

--- build/COPY_ROOT_EXTRA/opt/ai-dock/bin/build/layer1/init.sh
+++ build/COPY_ROOT_EXTRA/opt/ai-dock/bin/build/layer1/init.sh
@@ -2,40 +2,46 @@
 # This file will be sourced in init.sh
 # Namespace functions with provisioning_
 
-# https://raw.githubusercontent.com/ai-dock/stable-diffusion-webui/main/config/provisioning/default.sh
-
-### Edit the following arrays to suit your workflow - values must be quoted and separated by newlines or spaces.
-
-# Download and prepare the replacement files from the main branch
+# Clone and synchronize the Forge repository with the stable-diffusion-webui directory
 git clone -b main https://github.com/lllyasviel/stable-diffusion-webui-forge /workspace/stable-diffusion-webui-forge/
 rsync -avzh /workspace/stable-diffusion-webui-forge/ /workspace/stable-diffusion-webui/
 
-DISK_GB_REQUIRED=200
+### Edit the following arrays to suit your workflow - values must be quoted and separated by newlines or spaces.
+
+DISK_GB_REQUIRED=100
 
 MAMBA_PACKAGES=(
     "package1"
     "package2=version"
 )
-  
+
 PIP_PACKAGES=(
     "bitsandbytes==0.41.2.post2"
+    "ezsynth"
+    "numexpr"
+    "matplotlib"
+    "pandas"
+    "av"
+    "pims"
+    "imageio_ffmpeg"
+    "rich"
+    "gdown"
 )
 
 EXTENSIONS=(
     "https://github.com/lllyasviel/ControlNet-v1-1-nightly"
-    "https://github.com/deforum-art/sd-forge-deforum"
+    "https://github.com/deforum-art/sd-forge-deforum.git"
     "https://github.com/VBVerduijn/sd-webui-mov2mov"
+    "https://github.com/foglerek/SD-CN-Animation"
 )
 
 CHECKPOINT_MODELS=(
     "https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.ckpt"
-    #"https://huggingface.co/stabilityai/stable-diffusion-2-1/resolve/main/v2-1_768-ema-pruned.ckpt"
-    #"https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors"
-    #"https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0.safetensors"
+    "https://huggingface.co/lllyasviel/fav_models/resolve/main/fav/juggernautXL_v8Rundiffusion.safetensors"
 )
 
 LORA_MODELS=(
-    #"https://civitai.com/api/download/models/16576"
+    "https://civitai.com/api/download/models/16576"
 )
 
 VAE_MODELS=(
@@ -51,87 +57,109 @@ ESRGAN_MODELS=(
 )
 
 CONTROLNET_MODELS=(
-    "https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11f1e_sd15_tile.pth"
-    "https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11f1e_sd15_tile.yaml"
+    "https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/control_canny-fp16.safetensors"
+    "https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/control_depth-fp16.safetensors"
+    "https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/control_scribble-fp16.safetensors"
+    "https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/t2iadapter_depth-fp16.safetensors"
+    "https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/t2iadapter_sketch-fp16.safetensors"
 )
 
 ### DO NOT EDIT BELOW HERE UNLESS YOU KNOW WHAT YOU ARE DOING ###
 
-function provisioning_start() {
+function build_extra_start() {
     source /opt/ai-dock/etc/environment.sh
-    DISK_GB_AVAILABLE=$(( $(df --output=avail -m "${WORKSPACE}" | tail -n1) / 1000 ))
-    DISK_GB_USED=$(( $(df --output=used -m "${WORKSPACE}" | tail -n1) / 1000 ))
-    DISK_GB_ALLOCATED=$(( DISK_GB_AVAILABLE + DISK_GB_USED ))
-    provisioning_print_header
-    provisioning_get_mamba_packages
-    provisioning_get_pip_packages
-    provisioning_get_extensions
-    provisioning_get_models "${WORKSPACE}/stable_diffusion/models/Stable-diffusion" "${CHECKPOINT_MODELS[@]}"
-    provisioning_get_models "${WORKSPACE}/stable_diffusion/models/lora" "${LORA_MODELS[@]}"
-    provisioning_get_models "${WORKSPACE}/stable_diffusion/models/controlnet" "${CONTROLNET_MODELS[@]}"
-    provisioning_get_models "${WORKSPACE}/stable_diffusion/models/vae" "${VAE_MODELS[@]}"
-    provisioning_get_models "${WORKSPACE}/stable_diffusion/models/ESRGAN" "${ESRGAN_MODELS[@]}"
-     
-    PLATFORM_FLAGS=""
-    if [[ "${XPU_TARGET}" = "CPU" ]]; then
-        PLATFORM_FLAGS="--use-cpu all --skip-torch-cuda-test --no-half"
-    fi
-    PROVISIONING_FLAGS="--skip-python-version-check --xformers --cuda-malloc --no-half --no-half-vae --autolaunch"
-    FLAGS_COMBINED="${PLATFORM_FLAGS} $(cat /etc/a1111_webui_flags.conf) ${PROVISIONING_FLAGS}"
-    
-    # Start and exit because webui will probably require a restart
+    build_extra_get_mamba_packages
+    build_extra_get_pip_packages
+    build_extra_get_extensions
+    build_extra_get_models \
+        "/opt/storage/stable_diffusion/models/ckpt" \
+        "${CHECKPOINT_MODELS[@]}"
+    build_extra_get_models \
+        "/opt/storage/stable_diffusion/models/lora" \
+        "${LORA_MODELS[@]}"
+    build_extra_get_models \
+        "/opt/storage/stable_diffusion/models/controlnet" \
+        "${CONTROLNET_MODELS[@]}"
+    build_extra_get_models \
+        "/opt/storage/stable_diffusion/models/vae" \
+        "${VAE_MODELS[@]}"
+    build_extra_get_models \
+        "/opt/storage/stable_diffusion/models/esrgan" \
+        "${ESRGAN_MODELS[@]}"
+   
     cd /opt/stable-diffusion-webui && \
-    micromamba run -n webui -e LD_PRELOAD=libtcmalloc.so python launch.py ${FLAGS_COMBINED}
-    provisioning_print_end
+        micromamba run -n webui -e LD_PRELOAD=libtcmalloc.so python launch.py \
+        --use-cpu all \
+        --skip-torch-cuda-test \
+        --skip-python-version-check \
+        --no-download-sd-model \
+        --do-not-download-clip \
+        --no-half \
+        --port 11404 \
+        --exit
+    
+    # Ensure pytorch hasn't been clobbered
+    $MAMBA_DEFAULT_RUN python /opt/ai-dock/tests/assert-torch-version.py
 }
 
-function provisioning_get_mamba_packages() {
-    if [[ -n "${MAMBA_PACKAGES}" ]]; then
-        micromamba install -n webui "${MAMBA_PACKAGES[@]}"
+function build_extra_get_mamba_packages() {
+    if [[ -n $MAMBA_PACKAGES ]]; then
+        $MAMBA_INSTALL -n webui ${MAMBA_PACKAGES[@]}
     fi
 }
 
-function provisioning_get_pip_packages() {
-    if [[ -n "${PIP_PACKAGES}" ]]; then
-        micromamba run -n webui pip install "${PIP_PACKAGES[@]}"
+function build_extra_get_pip_packages() {
+    if [[ -n $PIP_PACKAGES ]]; then
+        micromamba run -n webui $PIP_INSTALL ${PIP_PACKAGES[@]}
     fi
 }
 
-function provisioning_get_extensions() {
+function build_extra_get_extensions() {
     for repo in "${EXTENSIONS[@]}"; do
         dir="${repo##*/}"
         path="/opt/stable-diffusion-webui/extensions/${dir}"
         requirements="${path}/requirements.txt"
-        if [[ -d "${path}" ]]; then
-            if [[ "${AUTO_UPDATE,,}" != "false" ]]; then
+        if [[ -d $path ]]; then
+            if [[ ${AUTO_UPDATE,,} != "false" ]]; then
                 printf "Updating extension: %s...\n" "${repo}"
-                (cd "${path}" && git pull)
-                if [[ -e "${requirements}" ]]; then
-                    micromamba run -n webui pip install -r "${requirements}"
+                ( cd "$path" && git pull )
+                if [[ -e $requirements ]]; then
+                    micromamba -n webui run ${PIP_INSTALL} -r "$requirements"
                 fi
             fi
         else
             printf "Downloading extension: %s...\n" "${repo}"
             git clone "${repo}" "${path}" --recursive
-            if [[ -e "${requirements}" ]]; then
-                micromamba run -n webui pip install -r "${requirements}"
+            if [[ -e $requirements ]]; then
+                micromamba -n webui run ${PIP_INSTALL} -r "${requirements}"
             fi
         fi
     done
 }
 
-function provisioning_get_models() {
-    if [[ -z "$2" ]]; then return 1; fi
-    dir="$1"
-    mkdir -p "${dir}"
-    shift
-    if [[ "${DISK_GB_ALLOCATED}" -ge "${DISK_GB_REQUIRED}" ]]; then
+function build_extra_get_models() {
+    if [[ -n $2 ]]; then
+        dir="$1"
+        mkdir -p "$dir"
+        shift
         arr=("$@")
-    else
-        printf "WARNING: Low disk space allocation - Only the first model will be downloaded!\n"
-        arr=("$1")
+        
+        printf "Downloading %s model(s) to %s...\n" "${#arr[@]}" "$dir"
+        for url in "${arr[@]}"; do
+            printf "Downloading: %s\n" "${url}"
+            build_extra_download "${url}" "${dir}"
+            printf "\n"
+        done
     fi
-    
-    printf "Downloading %s model(s) to %s...\n" "${#arr[@]}" "${dir}"
-    for url in "${arr[@]}"; do
-        printf "
+}
+
+# Download from $1 URL to $2 file path
+function build_extra_download() {
+    wget -qnc --content-disposition --show-progress -e dotbytes="${3:-4M}" -P "$2" "$1"
+}
+
+umask 002
+build_extra_start
+fix-permissions.sh -o container
+rm /etc/ld.so.cache
+ldconfig
